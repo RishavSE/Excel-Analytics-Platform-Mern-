da607037e481364369af97f28dcea4502669c5ac
Rishav Kumar Singh <rishavse06@gmail.com>
2025-07-30 01:52:30 +0530

    Update main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { AuthProvider } from "./Appcontext"; 
+import { AuthProvider } from "./Appcontext";
+import { BrowserRouter } from "react-router-dom";  // ✅ import this
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>     {/* ✅ wrap your app */}
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
