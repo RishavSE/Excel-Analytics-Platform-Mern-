edcfd231ba78f69069021f4ef57be214318e00de
Rishav Kumar Singh <rishavse06@gmail.com>
2026-01-30 12:07:09 +0530

    Update bycrypt-test.ts

--- mern-auth-secure/bycrypt-test.ts
+++ mern-auth-secure/bycrypt-test.ts
@@ -4,7 +4,7 @@ const password = "1111";
 const hash = "$2a$10$49iqUAhQN7zf.26OzB9Rwe6oIjM4h.bqxDcRFqBCEZ8ainljZugs.";
 
 (async () => {
-  console.log("🧪 DB Hash length:", hash.length);
+  console.log(" DB Hash length:", hash.length);
 
   const result = await bcrypt.compare(password, hash);
   console.log("Expected: true | Got:", result);
