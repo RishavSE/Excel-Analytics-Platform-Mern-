f6477dcdc1fbb793eb6963b0151645c4744db25a
Rishav Kumar Singh <rishavse06@gmail.com>
2026-01-30 12:04:18 +0530

    Update indexs.ts

--- mern-auth-secure/express-backend-jwt/indexs.ts
+++ mern-auth-secure/express-backend-jwt/indexs.ts
@@ -10,12 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ✅ Test route to confirm routing works
-// app.get("/ping", (req: Request, res: Response) => {
-//   res.send("pong");
-// });
 
-// ✅ Mount /api/login
 app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 5000;
